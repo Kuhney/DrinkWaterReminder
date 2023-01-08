459e83ce8c961867f19d6ad5c96c8f6aa8177793
Kuhney <42678994+Kuhney@users.noreply.github.com>
2023-01-08 13:04:01 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// 30-40 Milliliter pro Kilogramm Körpergewicht
+
+#include "main.h"     // Main header
+
+#include <Arduino.h>  // C system headers
+// C++ standard library headers (without file extension), e.g., <algorithm>, <cstddef>.
+
+// Other libraries' .h files.
+
+void setup(){
+
+}
+
+void loop(){
+  
+}
